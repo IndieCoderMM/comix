f8ba3aba20f797433ae3128c5460f6289c2c5d08
IndieCoderMM <hthant00chk@gmail.com>
2025-02-02 22:14:16 +0630

    Modify skeletons

--- features/stats/components/skeletons/repo-stats.tsx
+++ features/stats/components/skeletons/repo-stats.tsx
@@ -1,6 +1,6 @@
 const RepoStatSkeleton = () => {
   return (
-    <div className="grid w-full animate-pulse grid-cols-2 gap-2 bg-card md:grid-cols-4">
+    <div className="grid w-full animate-pulse grid-cols-2 gap-2 md:grid-cols-4">
       <div className="">
         <div className="h-24 rounded bg-card"></div>
       </div>
